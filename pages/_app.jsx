1115e4f6c8e1b82f573549b48cdf6f227aded7fa
Subin Lee <chandar.sblee@gmail.com>
2022-11-05 15:32:58 +0900

    [Chore] Change container spacing

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import "../styles/global.css";
 
 export default function SubuxApp({ Component, pageProps }) {
   return (
-    <div className="container px-16 py-8 mx-auto">
+    <div className="container px-8 sm:px-16 py-8 mx-auto">
       <Component {...pageProps} />
     </div>
   );
