c4c808a3d0982409a1d03be62e063a96f6288f84
Subin Lee <chandar.sblee@gmail.com>
2022-11-08 22:25:16 +0900

    [Chore] Move next.js to qwik

--- pages/_app.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/global.css";
-
-export default function SubuxApp({ Component, pageProps }) {
-  return (
-    <div className="container px-8 sm:px-16 py-8 mx-auto">
-      <Component {...pageProps} />
-    </div>
-  );
-}
