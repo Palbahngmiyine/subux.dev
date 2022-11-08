f2254d7dd61055dffb73e41eccf3f16749d50c97
Subin Lee <chandar.sblee@gmail.com>
2022-11-08 22:36:01 +0900

    [Chore] format files, change README.md

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,9 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
     <div class="container px-8 sm:px-16 py-8 mx-auto">
-      <Slot />        
+      <Slot />
     </div>
   );
 });
