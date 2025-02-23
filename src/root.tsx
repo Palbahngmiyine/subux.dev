b070582784463909599f6c8209fec7f7514354c1
Subin Lee <chandar.sblee@gmail.com>
2025-02-23 22:05:34 +0900

    Format files

--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,12 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+} from "@builder.io/qwik-city";
+import { RouterHead } from "./components/router-head/router-head";
+
+import "./global.css";
 
 export default component$(() => {
   /**
@@ -17,12 +19,12 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet='utf-8' />
-        <link rel='manifest' href='/manifest.json' />
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
         <RouterHead />
         <ServiceWorkerRegister />
       </head>
-      <body lang='ko'>
+      <body lang="ko">
         <RouterOutlet />
       </body>
     </QwikCityProvider>
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,6 @@
-import { component$, Slot } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-import type { RequestHandler } from '@builder.io/qwik-city';
+import { component$, Slot } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -21,7 +21,7 @@ export const useServerTimeLoader = routeLoader$(() => {
 
 export default component$(() => {
   return (
-    <main class='container px-2 md:px-8 py-8 mx-auto'>
+    <main class="container px-2 md:px-8 py-8 mx-auto">
       <Slot />
     </main>
   );
