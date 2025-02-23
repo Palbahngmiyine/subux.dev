f9fb53bc0320aa99828f7b122d3651ab0410a6cb
Subin Lee <chandar.sblee@gmail.com>
2025-02-23 21:58:50 +0900

    Update entry.netlify-edge.tsx:
    interface eslint disable

--- src/entry.netlify-edge.tsx
+++ src/entry.netlify-edge.tsx
@@ -16,6 +16,7 @@ import { manifest } from "@qwik-client-manifest";
 import render from "./entry.ssr";
 
 declare global {
+  // eslint-disable-next-line
   interface QwikCityPlatform extends PlatformNetlify {}
 }
 
